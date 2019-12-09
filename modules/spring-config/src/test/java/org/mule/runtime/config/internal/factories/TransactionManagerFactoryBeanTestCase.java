492e6aa6502682ca02958079bed1176a7afb1cbb
Rodro <rodrigo.merino@mulesoft.com>
2019-12-08 23:01:12 -0300

    tests revert

--- modules/spring-config/src/test/java/org/mule/runtime/config/internal/factories/TransactionManagerFactoryBeanTestCase.java
+++ modules/spring-config/src/test/java/org/mule/runtime/config/internal/factories/TransactionManagerFactoryBeanTestCase.java
@@ -10,12 +10,12 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import org.mule.runtime.core.internal.context.DefaultMuleContext;
-import org.mule.runtime.config.internal.factories.TransactionManagerFactoryBean;
 import org.mule.runtime.core.api.context.DefaultMuleContextFactory;
+import org.mule.runtime.core.internal.context.DefaultMuleContext;
 import org.mule.tck.config.TestServicesConfigurationBuilder;
 import org.mule.tck.core.internal.config.builders.DefaultsConfigurationBuilder;
 import org.mule.tck.junit4.AbstractMuleTestCase;
+import org.mule.tck.junit4.SimpleRegistryConfigurationBuilder;
 import org.mule.tck.testmodels.mule.TestTransactionManagerFactory;
 
 import javax.transaction.TransactionManager;
@@ -28,7 +28,8 @@ public class TransactionManagerFactoryBeanTestCase extends AbstractMuleTestCase 
   @Test
   public void registerTransactionManager() throws Exception {
     DefaultMuleContext context =
-        (DefaultMuleContext) new DefaultMuleContextFactory().createMuleContext(new TestServicesConfigurationBuilder(),
+        (DefaultMuleContext) new DefaultMuleContextFactory().createMuleContext(new SimpleRegistryConfigurationBuilder(),
+                                                                               new TestServicesConfigurationBuilder(),
                                                                                new DefaultsConfigurationBuilder());
 
     TransactionManagerFactoryBean txMgrFB = new TransactionManagerFactoryBean();
